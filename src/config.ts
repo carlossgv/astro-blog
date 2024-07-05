a81a69a13a185b3a8e6f50af4fa13a421956e76e
Carlos Gonzalez <carlossgv@gmail.com>
2024-07-05 12:50:03 -0600

    chore: add github pages workflow

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://carlossgv.github.io", // replace this with your deployed domain
+  base: "astro-blog", // replace this with your deployed domain
   author: "Sat Naing",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "AstroPaper",
